d56a9e3c247e2f6e637f7ad6366187d99dfbcfd9
ExplodedUrchin4 <82523333+ExplodedUrchin4@users.noreply.github.com>
2021-04-21 15:51:10 -0400

    Add files via upload

---
+++ connexion-bd.php
@@ -0,0 +1,18 @@
+<?php
+    include('param.inc.php');
+    
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    );
+
+    try {
+        $conn = new PDO("mysql:host=$dbHost;dbname=$dbNomBD", $dbUser, $dbMotPasse, $options);
+    }
+    catch (PDOException $e) {
+        exit( "Erreur lors de la connexion à la BD: ".$e->getMessage());
+     }
+
+
+?>
---
+++ param.inc.php
@@ -0,0 +1,6 @@
+<?php
+	$dbHost = 'localhost';
+	$dbNomBD = 'tp2_simong_williamg';
+	$dbUser = 'webphp';
+	$dbMotPasse = 'qwerty123';
+?>
